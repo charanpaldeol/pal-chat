ba8658338ab808bde871c99cbcbb3988389cb890
charanpaldeol <63482842+charanpaldeol@users.noreply.github.com>
2025-04-01 10:49:27 -0400

    Update signalService.js

--- server/services/signalService.js
+++ server/services/signalService.js
@@ -1,1 +1,14 @@
+// server/services/signalService.js
+
+const inMemoryStore = new Map();
+
+// Save user's Signal keys
+exports.storeKeys = (username, keys) => {
+  inMemoryStore.set(username, keys);
+};
+
+// Get stored keys for a user
+exports.getKeys = (username) => {
+  return inMemoryStore.get(username);
+};
 
