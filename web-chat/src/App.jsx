81ea62969fc4905cf01893e49ad9baed705c181c
charanpaldeol <63482842+charanpaldeol@users.noreply.github.com>
2025-04-02 09:45:47 -0400

    Update App.jsx

--- web-chat/src/App.jsx
+++ web-chat/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import * as libsignal from 'libsignal';
 
 import { IndexedDBSignalStore as SignalStore } from "./services/indexedSignalStore";
-import * as libsignal from '@privacyresearch/libsignal-protocol-javascript';
+// import * as libsignal from '@privacyresearch/libsignal-protocol-javascript';
 
 import ChatApp from "./ChatApp";
 
