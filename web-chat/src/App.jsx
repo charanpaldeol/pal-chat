f301f827694939552230b66fde5aa6f9d5c4e938
charanpaldeol <63482842+charanpaldeol@users.noreply.github.com>
2025-04-02 10:06:29 -0400

    Update App.jsx

--- web-chat/src/App.jsx
+++ web-chat/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
-import ChatPage from './pages/ChatPage';
+//import ChatPage from './pages/ChatPage';
 
 function App() {
   return (
