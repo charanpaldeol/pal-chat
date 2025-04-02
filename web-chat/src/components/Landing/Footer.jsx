749b11b31eacdd3f6a84ecd5d6a136abf101ddc9
charanpaldeol <63482842+charanpaldeol@users.noreply.github.com>
2025-04-02 09:59:41 -0400

    Update Footer.jsx

--- web-chat/src/components/Landing/Footer.jsx
+++ web-chat/src/components/Landing/Footer.jsx
@@ -1,1 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="py-8 bg-white text-center shadow-inner">
+      <p className="text-gray-500">© {new Date().getFullYear()} Pal Chat. All rights reserved.</p>
+    </footer>
+  );
+}
 
