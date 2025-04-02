2f3650bb547c94dac8eb365a55b61faf2c2f2ed5
charanpaldeol <63482842+charanpaldeol@users.noreply.github.com>
2025-04-02 11:29:54 -0400

    Update LandingPage.tsx

--- web-chat/src/pages/LandingPage.tsx
+++ web-chat/src/pages/LandingPage.tsx
@@ -1,47 +1,45 @@
-import { forwardRef, type ComponentType } from "react"
-import { createStore } from "https://framer.com/m/framer/store.js@^1.0.0"
-import { randomColor } from "https://framer.com/m/framer/utils.js@^0.9.0"
+import { forwardRef, type ComponentType } from "react";
+import { createStore } from "https://framer.com/m/framer/store.js@^1.0.0";
+import { randomColor } from "https://framer.com/m/framer/utils.js@^0.9.0";
 
-// Learn more: https://www.framer.com/developers/overrides/
+// Store to hold the background color state
+const useColorStore = createStore({
+  background: "#0099FF",
+});
 
-const useStore = createStore({
-    background: "#0099FF",
-})
-
-export function withRotate(Component): ComponentType {
-    return forwardRef((props, ref) => {
-        return (
-            <Component
-                ref={ref}
-                {...props}
-                animate={{ rotate: 90 }}
-                transition={{ duration: 2 }}
-            />
-        )
-    })
+// Rotate the component 90 degrees on mount
+export function withRotate<T>(Component: ComponentType<T>): ComponentType<T> {
+  return forwardRef<any, T>((props, ref) => (
+    <Component
+      ref={ref}
+      {...props}
+      animate={{ rotate: 90 }}
+      transition={{ duration: 2 }}
+    />
+  ));
 }
 
-export function withHover(Component): ComponentType {
-    return forwardRef((props, ref) => {
-        return <Component ref={ref} {...props} whileHover={{ scale: 1.05 }} />
-    })
+// Slightly enlarge the component on hover
+export function withHover<T>(Component: ComponentType<T>): ComponentType<T> {
+  return forwardRef<any, T>((props, ref) => (
+    <Component ref={ref} {...props} whileHover={{ scale: 1.05 }} />
+  ));
 }
 
-export function withRandomColor(Component): ComponentType {
-    return forwardRef((props, ref) => {
-        const [store, setStore] = useStore()
+// Randomize background color on click
+export function withRandomColor<T>(Component: ComponentType<T>): ComponentType<T> {
+  return forwardRef<any, T>((props, ref) => {
+    const [store, setStore] = useColorStore();
 
-        return (
-            <Component
-                ref={ref}
-                {...props}
-                animate={{
-                    background: store.background,
-                }}
-                onClick={() => {
-                    setStore({ background: randomColor() })
-                }}
-            />
-        )
-    })
+    return (
+      <Component
+        ref={ref}
+        {...props}
+        animate={{ background: store.background }}
+        onClick={() => {
+          setStore({ background: randomColor() });
+        }}
+      />
+    );
+  });
 }
